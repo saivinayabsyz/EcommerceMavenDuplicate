ccf8694d4c65e1aff225b2059b5f35aa8445bb39
saivinayabsyz <30690019+saivinayabsyz@users.noreply.github.com>
2018-10-22 16:03:09 +0530

    Update LoginService.java

--- src/main/java/com/absyz/service/LoginService.java
+++ src/main/java/com/absyz/service/LoginService.java
@@ -5,7 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-
+//libraries
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
